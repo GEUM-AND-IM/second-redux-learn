0a7d73733f2cf6faf64427320a3ed5e632fe1ed2
ldh3907 <ldh165163@gmail.com>
2022-01-21 01:38:35 +0900

    init

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,30 @@
+import Counter from "../components/Counter";
+import { useSelector, useDispatch } from "react-redux";
+import { decrease, increase, setDiff } from "../modules/counter";
+
+const CounterContainer = () => {
+  const { number, diff } = useSelector((state) => ({
+    number: state.counter.number,
+    diff: state.counter.diff,
+  }));
+
+  const dispatch = useDispatch();
+
+  const onIncrease = () => dispatch(increase());
+
+  const onDecrease = () => dispatch(decrease());
+
+  const onSetDiff = (diff) => dispatch(setDiff(diff));
+
+  return (
+    <Counter
+      number={number}
+      diff={diff}
+      onDecrease={onDecrease}
+      onIncrease={onIncrease}
+      onSetDiff={onSetDiff}
+    />
+  );
+};
+
+export default CounterContainer;
